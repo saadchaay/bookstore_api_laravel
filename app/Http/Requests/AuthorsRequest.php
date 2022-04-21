85a6caa7c3952b5079b7511dde9eeec17af661df
saadchaay <chaaysaad@gmail.com>
2022-04-21 15:52:18 +0000

    add new condition in AuthorsRequest.php file

--- app/Http/Requests/AuthorsRequest.php
+++ app/Http/Requests/AuthorsRequest.php
@@ -13,7 +13,7 @@ class AuthorsRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,7 @@ class AuthorsRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => 'required|unique:authors|max:255'
         ];
     }
 }
