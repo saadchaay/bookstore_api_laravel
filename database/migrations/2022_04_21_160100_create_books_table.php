023754ee5abdc1b48497c01bbf12e3e0dc7ffa65
saadchaay <chaaysaad@gmail.com>
2022-04-21 17:08:03 +0000

    add new migration

--- database/migrations/2022_04_21_160100_create_books_table.php
+++ database/migrations/2022_04_21_160100_create_books_table.php
@@ -15,6 +15,9 @@ class CreateBooksTable extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('publication_year');
             $table->timestamps();
         });
     }
