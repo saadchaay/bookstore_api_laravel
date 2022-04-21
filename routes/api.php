94f5f3283d47a5d238a4d514a9a027596bf44825
saadchaay <chaaysaad@gmail.com>
2022-04-21 14:25:40 +0000

    config the Author model

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthorsController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -18,5 +18,7 @@ Route::middleware('auth:api')->prefix('v1')->group( function(){
     Route::get('/user', function (Request $request){
         return $request->user();
     });
+
+    Route::get('authors/{author}', [AuthorsController::class, 'show']);
 });
 
