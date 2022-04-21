e798f9a6b710ec3c393b1dea5481eddc9e6fc355
saadchaay <chaaysaad@gmail.com>
2022-04-21 14:04:41 +0000

    add another route with middleware

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->prefix('v1')->group( function(){
+    Route::get('/user', function (Request $request){
+        return $request->user();
+    });
 });
+
